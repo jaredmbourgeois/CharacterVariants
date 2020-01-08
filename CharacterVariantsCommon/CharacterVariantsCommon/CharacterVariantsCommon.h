74151fb33c533bc16d542b9e16bf2ee58177f2a3
Jared Bourgeois <mail@jaredbourgeois.com>
2020-01-07 23:59:02 -0500

    Misc project cleanup.

---
+++ CharacterVariantsCommon/CharacterVariantsCommon/CharacterVariantsCommon.h
@@ -0,0 +1,17 @@
+//
+//  CharacterVariantsCommon.h
+//  CharacterVariantsCommon
+//
+//  Created by  Jared on 12/22/19.
+//  Copyright © 2019 Jared Bourgeois. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CharacterVariantsCommon.
+FOUNDATION_EXPORT double CharacterVariantsCommonVersionNumber;
+
+//! Project version string for CharacterVariantsCommon.
+FOUNDATION_EXPORT const unsigned char CharacterVariantsCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CharacterVariantsCommon/PublicHeader.h>
---
+++ CharacterVariantsCommon/CharacterVariantsCommon/Swift/Protocols.swift
@@ -0,0 +1,23 @@
+//
+//  Protocols.swift
+//
+//  Created by Jared Bourgeois on 8/10/19.
+//  Copyright © 2019 jaredmbourgeois. All rights reserved.
+//
+
+import UIKit
+
+public protocol Colorable {
+    func update(mainColor: UIColor?, textColor: UIColor?) -> Void
+}
+
+public protocol Dismissable {
+    var dismissesOnTouch: Bool? { get set }
+    var dismissingSplitViewController: UISplitViewController? { get set }
+    func dismiss() -> Void
+}
+
+public protocol Sizable {
+    static func number(from size: Format.Size) -> NSNumber
+    static func size(from number: NSNumber) -> Format.Size
+}
